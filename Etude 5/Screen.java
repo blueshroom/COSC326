a4e17807ad741aa7c24db7a6f85d4047166bceaa
Gavin Trebilcock <gavo.t@hotmail.com>
2017-08-02 23:50:42 +1200

    no message

--- Etude 5/Screen.java
+++ Etude 5/Screen.java
@@ -6,26 +6,31 @@ import java.util.ArrayList;
 public class Screen extends JPanel {
   
   public ArrayList<double[]> squares = new ArrayList<double[]>(); 
-  public double dubScale;
-  public int scale;
-  public int size;
-
+  public static double dubScale;
+  public static int initialScale;
+  public static int scale;
+  public static int size;
+  public static int r;
+  public static int g;
+  public static int b;
+  
   public Screen(ArrayList<double[]> squares, int size) {
-   //work out the size that the first square can be.
-   double totalPercent = 0;
-   for(int i = 0; i < squares.size(); i++){
-        totalPercent += squares.get(i)[0];
-   }
-   double hypot = Math.sqrt(((size*size) + (size*size)));
-   System.out.println("hypot: " + hypot);
-   System.out.println("percent: " + totalPercent);
-   dubScale = hypot/totalPercent;
-   //changing the 1% hypot length into the side lengths of the 1% square.
-   dubScale = Math.sqrt((dubScale*dubScale)/2);
-   //scale is the length and height of a square with 1% scale.
-   scale = Integer.valueOf((int) Math.round(dubScale));
-   System.out.println("scale: " + scale);
-
+    //work out the size that the first square can be.
+    double totalPercent = 0;
+    for(int i = 0; i < squares.size(); i++){
+      totalPercent += squares.get(i)[0];
+    }
+    double hypot = Math.sqrt(((size*size) + (size*size)));
+    System.out.println("hypot: " + hypot);
+    System.out.println("percent: " + totalPercent);
+    dubScale = hypot/totalPercent;
+    //changing the 1% hypot length into the side lengths of the 1% square.
+    dubScale = Math.sqrt((dubScale*dubScale)/2);
+    //scale is the length and height of a square with 1% scale.
+    initialScale = Integer.valueOf((int) Math.round(dubScale));
+    scale = initialScale;
+    System.out.println("scale: " + initialScale);
+    
 //   sorting the quilt patterns. 
 //   for(int i = 0; i < squares.size(); i++){
 //        double currentValue = squares.get(i)[0];
@@ -42,64 +47,101 @@ public class Screen extends JPanel {
   }
   
   public void paint(Graphics draw) { 
-    int topL = 0;
-    int topR = 0;
-    int botL = 0;
-    int botR = 0;
     int x = 0;
     int y = 0;
     //for each square in the arrayList, draw it on the page.
-    for(int i = 0; i < squares.size(); i++){
-      double[] square = squares.get(i);
-      Integer r = Integer.valueOf((int) Math.round(square[1]));
-      Integer g = Integer.valueOf((int) Math.round(square[2]));
-      Integer b = Integer.valueOf((int) Math.round(square[3]));
-      draw.setColor(new Color(r, g, b));
-      //draw the first square.
-      if(i < 1){
-        System.out.println("drawing");
-        x = ((size/2)-(scale/2));
-        y = ((size/2)-(scale/2));
-        draw.fillRect(x, y, scale, scale);
-      } else {
-        //drawing the 4 squares clockwise.
-        for(int j = 0; j < 4; j++){
-          //finding the new dimesions for the next square.
-          scale = squares.get(0)[5]*square[0];
-          System.out.println("drawing");
-          switch(j) {
-            //topLSquare
-            case 0:
-              x = 
-              y = 
-              draw.fillRect(x, y, scale, scale);
-              break;
-            //topRSquare
-            case 1:
-              x = 
-              y =
-              draw.fillRect(x, y, scale, scale);
-              break;
-            //botRSquare
-            case 2:
-              x = 
-              y =
-              draw.fillRect(x, y, scale, scale);
-              break;
-            //botLSquare
-            case 3:
-              x = 
-              y =
-              draw.fillRect(x, y, scale, scale);
-              break;
-          }
-        }   
-      }
-      //saving the dimensions of the square to the ArrayList.
-      square[5] =  scale;
-      squares.set(i, square)
-    }
+    r = Integer.valueOf((int) Math.round(squares.get(0)[1]));
+    g = Integer.valueOf((int) Math.round(squares.get(0)[2]));
+    b = Integer.valueOf((int) Math.round(squares.get(0)[3]));
+    draw.setColor(new Color(r, g, b));
+    //draw the first square.
+    System.out.println("drawing");
+    x = ((size/2)-(scale/2));
+    y = ((size/2)-(scale/2));
+    draw.fillRect(x, y, scale, scale);
     
+    //finding the new dimesions for the next square.
+    scale = Integer.valueOf((int) Math.round(initialScale*squares.get(0)[0]));
+    
+    //starting the recursion for drawing the squares.
+    drawSquares(x, y, scale, draw, 1);
+    
+    
+    
+    
+  }
+  
+  public void drawSquares(int prevX, int prevY, int prevScale, Graphics draw, int set){
+    int x;
+    int y;
+    System.out.println(Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale)));
+    if(squares.size() > 1){
+      for(int i = 0; i < 4; i++){  
+        System.out.println("drawing loop");
+        switch(i) {
+          //topLSquare
+          case 0:
+            x = (prevX)-Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale))/2;
+            y = (prevY+prevScale)-Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale))/2;
+            r = Integer.valueOf((int) Math.round(squares.get(set)[1]));
+            g = Integer.valueOf((int) Math.round(squares.get(set)[2]));
+            b = Integer.valueOf((int) Math.round(squares.get(set)[3]));
+            draw.setColor(new Color(r, g, b));
+            draw.fillRect(x, y, Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale)), 
+                          Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale)));
+            if(squares.size()-1 > set){
+              drawSquares(x, y, Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale)), draw, set+1);
+            }
+            break;
+            //topRSquare
+          case 1:
+            x = (prevX+prevScale)-Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale))/2;
+            y = (prevY+prevScale)-Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale))/2;
+            r = Integer.valueOf((int) Math.round(squares.get(set)[1]));
+            g = Integer.valueOf((int) Math.round(squares.get(set)[2]));
+            b = Integer.valueOf((int) Math.round(squares.get(set)[3]));
+            draw.setColor(new Color(r, g, b));
+            draw.fillRect(x, y, Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale)), 
+                          Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale)));
+            if(squares.size()-1 > set){
+              set++;
+              drawSquares(x, y, Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale)), draw, set+1);
+            }
+            break;
+            //botRSquare
+          case 2:
+            x = (prevX+prevScale)-Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale))/2;
+            y = (prevY)-Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale))/2;
+            r = Integer.valueOf((int) Math.round(squares.get(set)[1]));
+            g = Integer.valueOf((int) Math.round(squares.get(set)[2]));
+            b = Integer.valueOf((int) Math.round(squares.get(set)[3]));
+            draw.setColor(new Color(r, g, b));
+            draw.fillRect(x, y, Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale)), 
+                          Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale)));
+            if(squares.size()-1 > set){
+              drawSquares(x, y, Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale)), draw, set+1);
+            }
+            break;
+            //botLSquare
+          case 3:
+            x = (prevX)-Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale))/2;
+            y = (prevY)-Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale))/2;
+            r = Integer.valueOf((int) Math.round(squares.get(set)[1]));
+            g = Integer.valueOf((int) Math.round(squares.get(set)[2]));
+            b = Integer.valueOf((int) Math.round(squares.get(set)[3]));
+            draw.setColor(new Color(r, g, b));
+            draw.fillRect(x, y, Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale)), 
+                          Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale)));
+            if(squares.size()-1 > set){
+              drawSquares(x, y, Integer.valueOf((int) Math.round(squares.get(set)[0]*initialScale)), draw, set+1);
+            }
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    System.out.println("here");
   }
   
   

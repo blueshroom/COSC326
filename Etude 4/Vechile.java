59e3ddd36b4172a71a231249e271da541dd430b1
blueshroom <gavo.t@hotmail.com>
2017-08-07 17:20:42 +1200

    Update Vechile.java

--- Etude 4/Vechile.java
+++ Etude 4/Vechile.java
@@ -1,26 +1,53 @@
-public class Vechile{
+public class Vechile {
   public static double desert =  2413.0;
   public static boolean finished = false;
   public static ArrayList<Stop> stops = new ArrayList<Stop>();
   
   public Vechicle(int amount) {
     double tank = 720.0;
-    double potential = 0;
+    double cans = 960.0;
+    double potential = tank/2;
+    //variable that we will change for testing.
     int dropCanAmount = amount;
+    int stopIndex = 0;
     double currentPos;
   }
   
   
   public static void main(String[] args) {
-    Vechile vechile = new Vechile(30);
+    Vechile vechile = new Vechile(4);
     while(!finished){
-      potentail = how far you can go - how far you have to travel back.
+      //have fuel to continue.
+      if(potential > 0){
         
-    }
-    
-    
-  }
+        
+      } else {
+        //ran out of fuel.
+        if(stops.contains(stopIndex+1)){
+          arrivedAtStop(vechile, stops.get(stopIndex+1));
+        }
+        //going back to the start
+        for(int i = stopIndex; i > 0; i--){
+          Stop stop = stops.get(stopIndex);
+          if(stop.fuel - stop.fuelRequired < 0){
+            System.out.println("NOT ENOUGH FUEL, ERROR");
+          } else {
+            stop.fuel -= stop.fuelRequired;
+          }
+        }
+        //fill up the car.
+        vechile.stopIndex = 0;
+        vechile.tank = 720;
+        vechile.cans = 960;
+        vechile.potential = tank/2;
+        //now at the start (stop[0]).
+      }
+           
+         
+           
   
+  public static void newStop(){
+  }
   
   public static void pickup(Stop stop, double amount){
     stop.takeFuel(amount);
@@ -31,38 +58,30 @@ public class Vechile{
   }
   
   
-  public static void driveForward(double pos, int currentPos){
-    potential += tank/2;
-    int i = 0; 
-    while(potential > 0){
-      if(stops.indexOf(i) != -1){
-        Stop stop = stops.get(i);
-        if(potential >= stops.position){
-          //go to stop.
-          currentPos = stop.position;
-          //removing the fuel that was used.
-          if(i > 0){
-            potential -= stops.get(i-1).position;
-            //stop.takeFuel(stop.position - stops.get(i-1).position));
-          } else {
-            potential -= stop.positon;
-          }
-          
-        }
+  public static void arrivedAtStop(Vechile vechile, Stop stop){
+    //if there is enough fuel at the stop, fill the tank with fuel that can be used
+    if(stop.fuel > stop.fuelRequried){
+      if((stop.fuel - stop.fuelRequired) < (720 - tank)){
+        vechile.tank += (stop.fuel - stop.fuelRequired);
+      } else {
+        stop.fuel -= (720 - vechile.tank);
+        vechile.tank = 720.0;
       }
-      potential -= 1;
+      //if there is not enough fuel to return to the previous stop, top it up before moving forward.
+    } else {
+      if(vechile.cans > stop.fuelRequired){
+        if(vechile.potential <= 0){
+          stop.fuel = vechile.cans;
+          vechile.cans = 0;
+        }
+        vechile.cans -= (stop.fuelRequired - stop.fuel);
+        stop.fuel += (stop.fuelRequired - stop.fuel);
+      }
     }
+    
   }
-  
-  
-  
-  
-  
-  
-}
 
 public static void driveBack(){
   //take note of where we need to pick up fuel to get back.
   //pick up fuel at correct spots and refuel when we get to the begginning.
 }
-  

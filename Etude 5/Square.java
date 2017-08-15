4a357ca156460f8e2dfc03a27bb2488e5007b58b
blueshroom <gavo.t@hotmail.com>
2017-08-15 15:11:39 +1200

    Create Square.java

---
+++ Etude 5/Square.java
@@ -0,0 +1,18 @@
+public class Square {
+  
+  int x;
+  int y;
+  int scale;
+  int r;
+  int g;
+  int b;
+  
+  public Square(int x, int y, int scale, int r, int g, int b){
+    this.x = x;
+    this.y = y;
+    this.scale = scale;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
+}

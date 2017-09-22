37685b8491eed5021d1015f24168162becda893a
Gavin Trebilcock <gavo.t@hotmail.com>
2017-09-22 16:30:51 +1200

    RedAndGreen working and ready for submit

--- Etude 10/RedAndGreenE.java
+++ Etude 10/RedAndGreenE.java
@@ -1,50 +1,56 @@
-import java.util.HashMap;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
+
 
 //possibly use this for SUPERSIZE it.
 public class RedAndGreenE{
+  //public static HashMap<Integer, ArrayList<Integer>> list = new HashMap<Integer, ArrayList<Integer>>();
   public static HashMap<Integer, Integer> factorList = new HashMap<Integer, Integer>();
   public static HashMap<Integer, Character> colours = new HashMap<Integer, Character>();
   public static StringBuffer output = new StringBuffer("");
-  public static int startNum = 1;
-  public static int endNum = 1000;
+  public static int startNum = 0;
+  public static int endNum = 0;
   
   public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
     long startTime = System.currentTimeMillis();
     
-    findNearFactorsE(startNum);
-      
-    System.out.println(output);
-    long endTime   = System.currentTimeMillis();
-    long totalTime = endTime - startTime;
-    System.out.println("MILLISECONDS: " + totalTime);
+    while(scan.hasNextInt()){
+      startNum = scan.nextInt();
+      endNum = scan.nextInt();
+    }
     
+    if(!(startNum == endNum)){   
+      for(int i = startNum; i <= endNum; i++){
+        findNearFactorsE(i);
+      }
+      System.out.println(output);
+      long endTime   = System.currentTimeMillis();
+      long totalTime = endTime - startTime;
+      //System.out.println("MILLISECONDS: " + totalTime);
+    }
   }
+
   
   
   
   public static void findNearFactorsE(int num){
-    HashMap<Integer, Integer> list;
-    list = factorList;
     int gCount = 0;
     int rCount = 0;
     if(num != 1){
-      list.put(1, 1);
+      factorList.put(1, 1);
       gCount++;
       for(int i = 2; i < (num/2+1); i++){
         int factor = num/i;
         //if it is a new factor
-        if(!list.containsValue(factor)){
-          list.put(list.size() + 1, factor);
-          if(colours.get(factor-1) == 'G'){
+        if(!factorList.containsValue(factor)){
+          factorList.put(factorList.size() + 1, factor);
+          if(colours.get(factor) == 'G'){
             gCount++;
           } else {
             rCount++;
           }
         }        
       }
-      
       if(gCount > rCount){
         colours.put(num, 'R');
         output.append('R');
@@ -56,12 +62,7 @@ public class RedAndGreenE{
       colours.put(num, 'G');
       output.append('G');
     }
-    
-    list = null;
-    if((num + 1) <= endNum){
-      findNearFactorsE(num+1);
-    }
+    factorList.clear();
   }
-  
   
 }

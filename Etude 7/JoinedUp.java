9239b819f9a4814cf1113e28ff544394646b9676
Gavin Trebilcock <gavo.t@hotmail.com>
2017-10-02 14:49:47 +1300

    Added more methods to JoinedUp and added a small dictionary (“text.txt”)

---
+++ Etude 7/JoinedUp.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class JoinedUp{
+  public static List<String> words = new ArrayList<String>();
+  public static int[] firstIndex = new int[26];
+
+  public static void main(String[] args){
+    
+    Scanner scan = new Scanner(System.in);
+    while(scan.hasNext()){
+      words.add(scan.next());
+    }
+    
+    Collections.sort(words);
+    
+    char subject = 'a';
+    firstIndex[subject-97] = 0;
+    for(int i = 0; i < words.size(); i++){
+      if(words.get(i).charAt(0) == subject+1){
+        subject += 1;
+        firstIndex[subject-97] = i; 
+      }
+    }
+    
+    for(int i = 0; i < 26; i++){
+      System.out.println(firstIndex[i]);
+    }
+    
+  }
+    
+  
+  class TreeNode {
+    private List<TreeNode> children = null;
+    private TreeNode parent = null;
+    private Integer value = null;
+    
+    public TreeNode(Integer value){
+      this.children = new ArrayList<TreeNode>();
+      this.value = value;
+    }
+    
+    public void addChild(TreeNode child){
+      child.parent = this;
+      children.add(child);
+      
+    }
+  }
+
+}

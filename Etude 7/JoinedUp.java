899cd5ebd314de2bf14e84d17ef879a0207283f2
Gavin Trebilcock <gavo.t@hotmail.com>
2017-10-04 11:18:32 +1300

    Made changes to Joined Up

--- Etude 7/JoinedUp.java
+++ Etude 7/JoinedUp.java
@@ -5,24 +5,26 @@ public class JoinedUp{
   public static List<ArrayList<String>> singleLinkedOptions = new ArrayList<ArrayList<String>>();
   public static int[] firstCharIndex = new int[26];
   public static String inputWord;
+  public static String finalWord;
 
   public static void main(String[] args){
+    inputWord = args[0];
+    finalWord = args[1];
     JoinedUp demo = new JoinedUp();
     demo.joinedUp();
   }
     
   public void joinedUp(){
     Scanner scan = new Scanner(System.in);
-    inputWord = scan.next();
     while(scan.hasNext()){
       String word = scan.next();
       words.add(word);
     }
     
     Collections.sort(words);
-    for(int i = 0; i < words.size(); i++){
-      System.out.println(words.get(i));
-    }
+//    for(int i = 0; i < words.size(); i++){
+//      System.out.println(words.get(i));
+//    }
     
     //find the indexs of the first letter of each word
     char subject = 'a';
@@ -40,12 +42,9 @@ public class JoinedUp{
     findLinks(inputWord, new ArrayList<String>());
     
     //printing the results
-    System.out.println(singleLinkedOptions.size());
-    for(int i = 0; i < singleLinkedOptions.size(); i++){
-      System.out.println("------------");
-      for(int j = 0; j < singleLinkedOptions.get(i).size(); j++){
-        System.out.println(singleLinkedOptions.get(i).get(j));
-      }
+    System.out.println("------------");
+    for(int j = 0; j < singleLinkedOptions.get(0).size(); j++){
+      System.out.println(singleLinkedOptions.get(0).get(j));
     }
   }
   
@@ -53,37 +52,61 @@ public class JoinedUp{
     ArrayList<String> linkedWords = prevLinkedWords;
     for(int i = 0; i < words.size(); i++){
       
-      //finding the singleJoinedLen
-      int singleJoinedLen = 0;
-      if(word.length() > words.get(i).length()){
-        singleJoinedLen = (words.get(i).length())/2;
-        if(words.get(i).length() % 2 == 0){
-          singleJoinedLen++;
+      if(!(linkedWords.contains(words.get(i)))){
+        
+        //finding the singleJoinedLen
+        int singleJoinedLen = 0;
+        if(word.length() > words.get(i).length()){
+          singleJoinedLen = (words.get(i).length())/2;
+//        if(words.get(i).length() % 2 == 0){
+//          singleJoinedLen++;
+//        }
+        } else {
+          singleJoinedLen = word.length()/2;
+//        if(word.length() % 2 == 0){
+//          singleJoinedLen++;
+//        }
         }
-      } else {
-        singleJoinedLen = word.length()/2;
-        if(word.length() % 2 == 0){
-          singleJoinedLen++;
+        
+        int pos;
+        boolean inputLarger;
+        //setting how deep we check similarities between the words.
+        if(word.length() > words.get(i).length()){
+          pos = words.get(i).length();
+          inputLarger = true;
+        } else {
+          pos = word.length();
+          inputLarger = false;
         }
+        
+        boolean finished = false;
+        while(!finished){
+          String subString = word.substring(word.length()-pos);
+          System.out.println(subString + ":" + words.get(i).substring(0, subString.length()));
+          if(!(subString.equals(words.get(i).substring(0, pos)))){
+            pos--;
+            if(pos < 1){
+              finished = true;
+            }
+          } else {
+            System.out.println(singleJoinedLen);
+            //if it qualifies to be a singleJoined link.
+            if(pos >= singleJoinedLen){
+              System.out.println("--------FOUND LINK------");
+              linkedWords.add(words.get(i));
+              if(words.get(i) == finalWord){
+                return;
+              } else {
+                findLinks(words.get(i), linkedWords);
+              }
+            }
+            finished = true;
+          }
+        }                                                     
       }
       
-      int pos = 0;
-      boolean finished = false;
-      while(finished){
-        if((word.charAt((word.length()-1)-pos)) == words.get(i).charAt(pos)){
-          System.out.println("WORD: " + word + " | " + "COMPRATOR: " + words.get(i));
-          System.out.println(word.charAt((word.length()-1)-pos) + " | " + words.get(i).charAt(pos));
-          pos++;
-        } else {
-          if(pos >= singleJoinedLen){
-            linkedWords.add(words.get(i));
-            findLinks(word, linkedWords);
-          }
-          finished = true;
-        }
-      }                                                     
+      singleLinkedOptions.add(linkedWords);
     }
-    singleLinkedOptions.add(linkedWords);
   }
     
   
